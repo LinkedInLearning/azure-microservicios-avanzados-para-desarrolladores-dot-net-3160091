ac516508aab2b8c1cc0ba6e4908e0d748592c5ac
Rodrigo Díaz Concha <rodrigo@rdiazconcha.com>
2021-10-01 01:27:25 -0500

    02_04f

---
+++ src/WisdomPetMedicine.Rescue.Api/IntegrationEvents/PetFlaggedForAdoptionIntegrationEvent.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace WisdomPetMedicine.Rescue.Api.IntegrationEvents
+{
+    public record PetFlaggedForAdoptionIntegrationEvent(Guid Id, string Name, string Breed, int Sex, string Color, DateTime DateOfBirth, string Species);
+}
